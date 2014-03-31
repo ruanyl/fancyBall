22f3bbee1b2076513ad97b25d551350fe31b425f
ruanyl <ruanyu1@gmail.com>
2014-03-31 18:05:48 +0300

    smooth animate

--- fancyBall.js
+++ fancyBall.js
@@ -4,7 +4,7 @@ var fancyBall = function(width, height) {
   this.balls = [];
   this.width = width || 480;
   this.height = height || 400;
-  this.lineDist = 60;
+  this.lineDist = 100;
 };
 
 fancyBall.prototype = {
@@ -35,17 +35,21 @@ fancyBall.prototype = {
     if (this.collide(obja, objb)) {
       obja.collide = "true";
       objb.collide = "true";
-      this.ctx.moveTo(obja.x, obja.y);
-      this.ctx.quadraticCurveTo(obja.x, obja.y+20, objb.x, objb.y);
-      this.ctx.stroke();
     }
   },
   collide: function(obja, objb) {
     var minDist = obja.r + objb.r + this.lineDist;
     var distX = obja.x - objb.x;
-    var distY = obja.y -objb.y;
-    var dist = distX * distX + distY * distY;
-    if(dist <= minDist * minDist) {
+    var distY = obja.y - objb.y;
+    var dist_2 = distX * distX + distY * distY;
+    //开根号
+    var dist = Math.pow(dist_2, 1 / 2);
+    if (dist_2 <= minDist * minDist) {
+      this.ctx.moveTo(Math.floor(obja.x)+0.5, Math.floor(obja.y)+0.5);
+      this.ctx.strokeStyle = "rgba(134,160,213," + (1-dist/this.lineDist) + ")";
+      //this.ctx.quadraticCurveTo(obja.x, obja.y+40, objb.x, objb.y);
+      this.ctx.lineTo(Math.floor(objb.x)+0.5, Math.floor(objb.y)+0.5);
+      this.ctx.stroke();
       return true;
     } else {
       return false;
@@ -60,16 +64,11 @@ fancyBall.prototype = {
   draw: function() {
     this.ctx.clearRect(0, 0, this.width, this.height);
     for (var i = this.balls.length; i--;) {
-      this.ctx.beginPath();
-      this.ctx.arc(this.balls[i].x, this.balls[i].y, this.balls[i].r, 0, Math.PI * 2);
-      this.ctx.fillStyle = "#CCC";
-      /*if (this.balls[i].collide === "true") {*/
-        //this.balls[i].collide = "false";
-        //this.ctx.fillStyle = "#C00";
-      //} else {
-        //this.ctx.fillStyle = "#CCC";
-      /*}*/
       this.checkCollide();
+      this.ctx.fillStyle = "#86a0d5";
+      this.ctx.beginPath();
+      this.ctx.arc(this.balls[i].x, this.balls[i].y, this.balls[i].r, 0, Math.PI * 2, true);
+      this.ctx.closePath();
       this.ctx.fill();
     }
   },
@@ -90,14 +89,13 @@ fancyBall.prototype = {
   },
   update: function() {
     this.move();
-    //this.checkCollide();
     this.draw();
   },
   run: function() {
     var _this = this;
     this.timer = setInterval(function() {
       _this.update();
-    }, 1000 / 60);
+    }, 20);
   },
   init: function(num) {
     this.createBall(num);
@@ -111,16 +109,39 @@ var Ball = function(maxX, maxY, minR, maxR, maxV) {
   //this.y = Math.random() * maxY >> 0;
   //this.vx = Math.random() * maxV + 1 >> 0;
   this.vx = 0;
-  this.vy = -(Math.random() * maxV + 1) >> 0;
-  this.r = Math.random() * maxR >> 0;
+  this.vy = -1 * Math.random() - Math.random() - .05;
+  //this.r = Math.random() * maxR >> 0;
+  this.r = 1;
   this.collide = false;
   if (this.r < minR) {
     this.r = minR;
   }
 };
 
-var fancyBall = new fancyBall(960, 800);
+var viewportwidth;
+var viewportheight;
+
+// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+
+if (typeof window.innerWidth != 'undefined') {
+  viewportwidth = window.innerWidth,
+  viewportheight = window.innerHeight
+}
+
+// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth !=
+  'undefined' && document.documentElement.clientWidth != 0) {
+  viewportwidth = document.documentElement.clientWidth,
+  viewportheight = document.documentElement.clientHeight
+}
+
+// older versions of IE
+else {
+  viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
+  viewportheight = document.getElementsByTagName('body')[0].clientHeight
+}
+var fancyBall = new fancyBall(viewportwidth, viewportheight);
 fancyBall.initCanvas('myCanvas');
 if (fancyBall.canvas) {
-  fancyBall.init(20);
+  fancyBall.init(30);
 }
